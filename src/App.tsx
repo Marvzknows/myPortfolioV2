2865c158cfe413e68b7df9496a0407d7c7894e76
Marvin Lim <marvzknows03@gmail.com>
2024-12-16 09:03:01 +0800

    under development

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { TypeAnimation } from 'react-type-animation'
 import './App.css'
 import Experience from './components/experience/experience'
 import Header from './components/Header/Header'
@@ -29,7 +30,21 @@ function App() {
 
       <div className=' border-error-500 py-1 max-w-[1280px] mx-auto'>
         {/* ABOUT ME */}
-
+        <div className='border h-[50vh] flex flex-col items-center justify-center gap-6'>
+          <h1 className='font-bold'>THE FOLLOWING ARE STILL UNDER DEVELOPMENT</h1>
+        <TypeAnimation
+              sequence={[
+                "About me Section", // First text
+                2000, // Pause for 1 second
+                "Projects Section", // Second text
+                2000, // Pause for 1 second
+                "Contact me Section", // Third text
+                2000, // Pause for 1 second
+              ]}
+              speed={50}
+              repeat={Infinity}
+            />
+        </div>
         {/* MY PROJECTS */}
 
         {/* TESTIMONIAL (optional) */}
